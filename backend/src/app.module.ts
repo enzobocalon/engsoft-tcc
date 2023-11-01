24b37a6bc08d8b1f34b6df0f930f218df9395fcc
unknown <enzo.bocalon@hotmail.com>
2023-11-01 17:13:06 -0300

    fix: corrigido servidor nao deixando acessar as rotas dos pdfs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { DatabaseModule } from './shared/database/database.module';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { DocumentsModule } from './modules/documents/documents.module';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthModule, DatabaseModule, DocumentsModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public', 'documents'),
+      serveStaticOptions: {
+        index: false,
+      },
+      serveRoot: '/public/documents',
+    }),
+    AuthModule,
+    DatabaseModule,
+    DocumentsModule,
+  ],
   controllers: [],
   providers: [
     {
